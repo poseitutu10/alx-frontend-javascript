8cd4eadb12ddb098c2a671095f83d4b7b672c758
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-05-26 10:20:24 +0000

    type predicate in typescript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -35,8 +35,6 @@ class Teacher implements TeacherInterface {
   workTeacherTasks(): string {
     return `Getting to work`;
   }
-  
-
 }
 
 interface construct {
@@ -44,17 +42,38 @@ interface construct {
 }
 
 const createEmployee = (salary: number | string): Teacher | Director => {
-  if((typeof salary == "string")){
+  if (typeof salary == "string") {
     salary = salary.replace(/\D/g, "");
     salary = parseInt(salary, 10);
   }
-  if((typeof salary == "number") && salary < 500){
+  if (typeof salary == "number" && salary < 500) {
     return new Teacher();
+  } else if (typeof salary == "number" && salary >= 500) {
+    return new Director();
   }
-  else if((typeof salary == "number") && salary >= 500){
-    return new Director;
-  }
-}
+};
 
 const employeeInstance = createEmployee("$400");
-console.log(employeeInstance)
+console.log(employeeInstance);
+
+//Creating functions specific to employees
+
+const isDirector = (employees: Director | Teacher): employees is Director => {
+  return (employees as Director).workDirectorTasks !== undefined;
+};
+
+const result = isDirector(createEmployee(700));
+
+
+const executeWork = (employees: Director | Teacher): string => {
+  if ((employees as Director).workDirectorTasks) {
+    return (employees as Director).workDirectorTasks();
+  } else if ((employees as Teacher).workTeacherTasks) {
+    return (employees as Teacher).workTeacherTasks();
+  }
+};
+
+const ex1 = executeWork(createEmployee(200));
+const ex2 = executeWork(createEmployee(700));
+console.log(ex1);
+console.log(ex2)
